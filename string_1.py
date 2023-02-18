8f2c28fdbebdafa816732e5e5f11ef334b8d4b0d
asto nugroho <asto@giftano.com>
2023-02-18 07:57:52 +0700

    batch 2

---
+++ string_1.py
@@ -0,0 +1,25 @@
+_nama = "ini adalah text"
+_nomor = "90"
+# print(_nama)
+
+# check string or type data
+# dua argumen param 1 data / vars param ke dua type data
+_cek = isinstance(_nomor, int)
+
+# print(_cek)
+
+# penggabungan string
+
+# cara pertama
+print(_nama, _nomor)
+
+# cara kedua
+print(f'coba cara kedua {_nama} dan {_nomor}')
+
+
+# string concatenation
+print(_nama + _nomor)
+
+# string concatienation *
+
+print(50 * _nama)
